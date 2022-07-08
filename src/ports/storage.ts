2ca95f09158c004a05b4b0b2dd2b60a7e2917aa8
Rafael Doering <rafael.doering.soares@usp.br>
2022-07-08 00:16:32 -0300

    Delete unused files

--- src/ports/storage.ts
+++
@@ -1,9 +0,0 @@
-export default interface Storage<T> {
-  save(entity: T): Promise<void>;
-
-  findById(id: string): Promise<T>;
-
-  findAll(): Promise<T[]>;
-
-  delete(entity: T): Promise<void>;
-}

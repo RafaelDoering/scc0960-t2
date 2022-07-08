853939727f765422c9a717ef962fab0c2810f535
Rafael Doering <rafael.doering.soares@usp.br>
2022-07-08 00:45:38 -0300

    Refactor covid data processor

--- src/adapters/covid-data-processor.ts
+++ src/adapters/covid-data-processor.ts
@@ -17,51 +17,44 @@ export default class CovidDataProcessor {
     }
   }
 
-  getData(data: CountryData[]): void {
-    console.log("Os três países com os maiores valores de \"Confirmed\". Os nomes devem estar em ordem alfabética");
-    console.log(
-      data
-        .sort((countryData1, countryData2) => this.sort(countryData1.confirmedCases, countryData2.confirmedCases, { sort: { order: "desc" } }))
-        .slice(0, 3)
-        .sort((countryData1, countryData2) => this.sort(countryData1.name, countryData2.name, { sort: { order: "asc" } }))
-        .map(countryData => countryData.name)
-    );
+  getGreatestConfirmedCountries(data: CountryData[]): string[] {
+    return data
+      .sort((countryData1, countryData2) => this.sort(countryData1.confirmedCases, countryData2.confirmedCases, { sort: { order: "desc" } }))
+      .slice(0, 3)
+      .sort((countryData1, countryData2) => this.sort(countryData1.name, countryData2.name, { sort: { order: "asc" } }))
+      .map(countryData => countryData.name);
+  }
 
-    console.log("Dentre os dez países com maiores valores de \"Active\", a soma dos \"Deaths\" dos cinco países com menores valres de \"Confirmed\"");
-    console.log(
-      data
-        .sort((countryData1, countryData2) => this.sort(countryData1.activeCases, countryData2.activeCases, { sort: { order: "desc" } }))
-        .slice(0, 10)
-        .sort((countryData1, countryData2) => this.sort(countryData1.confirmedCases, countryData2.confirmedCases, { sort: { order: "asc" } }))
-        .slice(0, 5)
-        .reduce((deaths, countryData) => deaths += countryData.deaths, 0)
-    );
+  getDeathsFromCountriesWithLessConfirmedInGreatestActive(data: CountryData[]): number {
+    return data
+      .sort((countryData1, countryData2) => this.sort(countryData1.activeCases, countryData2.activeCases, { sort: { order: "desc" } }))
+      .slice(0, 10)
+      .sort((countryData1, countryData2) => this.sort(countryData1.confirmedCases, countryData2.confirmedCases, { sort: { order: "asc" } }))
+      .slice(0, 5)
+      .reduce((deaths, countryData) => deaths += countryData.deaths, 0);
+  }
 
-    console.log("O maior valor de \"Deaths\" entre os países do hemisfério sul.");
-    console.log(
-      data
-        .filter((countryData) => countryData.lng < 90)
-        .sort((countryData1, countryData2) => this.sort(countryData1.deaths, countryData2.deaths, { sort: { order: "desc" } }))
-        .slice(0, 1)
-        .map(countryData => countryData.deaths)
-        .at(0)
-    );
+  getGreatestDeathsInSouthernHemisphere(data: CountryData[]): number {
+    return data
+      .filter((countryData) => countryData.lng < 90)
+      .sort((countryData1, countryData2) => this.sort(countryData1.deaths, countryData2.deaths, { sort: { order: "desc" } }))
+      .slice(0, 1)
+      .map(countryData => countryData.deaths)
+      .at(0);
+  }
 
-    console.log("O maior valor de \"Deaths\" entre os países do hemisfério norte");
-    console.log(
-      data
-        .filter((countryData) => countryData.lng > 90)
-        .sort((countryData1, countryData2) => this.sort(countryData1.deaths, countryData2.deaths, { sort: { order: "desc" } }))
-        .slice(0, 1)
-        .map(countryData => countryData.deaths)
-        .at(0)
-    );
+  getGreatestDeathsInNorthernHemisphere(data: CountryData[]): number {
+    return data
+      .filter((countryData) => countryData.lng > 90)
+      .sort((countryData1, countryData2) => this.sort(countryData1.deaths, countryData2.deaths, { sort: { order: "desc" } }))
+      .slice(0, 1)
+      .map(countryData => countryData.deaths)
+      .at(0);
+  }
 
-    console.log("A soma de \"Active\" de todos os países em que \"Confirmed\" é maior o igual que 1.000.000");
-    console.log(
-      data
-        .filter((countryData) => countryData.confirmedCases > 1000000)
-        .reduce((activeCases, countryData) => activeCases += countryData.activeCases, 0)
-    );
+  getActiveCasesSumOfCountriesWithMoreThanAMillionConfirmedCases(data: CountryData[]): number {
+    return data
+      .filter((countryData) => countryData.confirmedCases > 1000000)
+      .reduce((activeCases, countryData) => activeCases += countryData.activeCases, 0);
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,21 @@ async function run() {
   const data = await covidDataMapper.map(fileData);
 
   const covidDataProcessor = new CovidDataProcessor();
-  covidDataProcessor.getData(data);
+
+  console.log("Os três países com os maiores valores de \"Confirmed\". Os nomes devem estar em ordem alfabética");
+  console.log(covidDataProcessor.getGreatestConfirmedCountries(data).join(", "));
+
+  console.log("Dentre os dez países com maiores valores de \"Active\", a soma dos \"Deaths\" dos cinco países com menores valores de \"Confirmed\"");
+  console.log(covidDataProcessor.getDeathsFromCountriesWithLessConfirmedInGreatestActive(data));
+
+  console.log("O maior valor de \"Deaths\" entre os países do hemisfério sul.");
+  console.log(covidDataProcessor.getGreatestDeathsInSouthernHemisphere(data));
+
+  console.log("O maior valor de \"Deaths\" entre os países do hemisfério norte");
+  console.log(covidDataProcessor.getGreatestDeathsInNorthernHemisphere(data));
+
+  console.log("A soma de \"Active\" de todos os países em que \"Confirmed\" é maior o igual que 1.000.000");
+  console.log(covidDataProcessor.getActiveCasesSumOfCountriesWithMoreThanAMillionConfirmedCases(data));
 }
 
 run();
